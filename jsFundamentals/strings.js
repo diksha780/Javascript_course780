982cf77898f9e858b9a4905057197ec9b0417d6b
Diksha Nangla <112332164+diksha780@users.noreply.github.com>
2024-07-17 15:21:33 +0000

    strings

---
+++ jsFundamentals/strings.js
@@ -0,0 +1,30 @@
+let name1="Diksha Rani"
+// *****************at()
+// console.log(name1.at(2)) //at index2
+
+// ************slice()
+// console.log(name1.slice(2,8))//starting from 2 , 8TH index doesnt included
+// console.log(name1.slice(2)) //one oarameter, will slixe out rest of the string
+// console.log(name1.slice(-2)) //gives last two characters of string
+// console.log(name1.slice(-4)) //slice out last four characters of string
+
+// console.log(name1.slice(2,-4))  //start from 2nd index and cut last 4  chars
+// console.log(name1.slice(2,-7))  //start from 2nd index and cut last 7  chars
+// console.log(name1.slice(2,-3))  //start from 2nd index and cut last 3 chars
+
+// console.log(name1.slice(-6,-3)) // start from  6th char (from last) and cut last 3chars
+// console.log(name1.slice(-5,-3)) // start from  5th char (from last) and cut last 3chars
+// console.log(name1.slice(-7,-2)) // start from  7th char (from last) and cut last 3chars
+// console.log(name1.slice(-8,-3)) // start from  8th char (from last) and cut last 3chars
+// console.log(name1.slice(-4,-3))
+console.log(name1.slice(-8,5)) //start from last 8th char till index 5 from start but 5 index char not included
+console.log(name1.slice(-4,8))//start from last 4th char till index 8 from start but 8 index char not included
+console.log(name1.slice(-9,8))//start from last 9th char till index 8 from start but 8 index char not included
+// // ************substring()
+
+// console.log(name1.substring(2,5)); //similar to slice()..start from 2nd index and 5th index doesn't included
+//negative values are treated as 0
+// console.log(name1.substring(2,5));
+
+
+
