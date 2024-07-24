41bab3c8727dd1480495232d9aac8ece9c4815a6
Diksha Nangla <112332164+diksha780@users.noreply.github.com>
2024-07-24 15:26:48 +0000

    math

--- jsFundamentals/numbers_math.js
+++ jsFundamentals/numbers_math.js
@@ -1,11 +1,11 @@
-// let balance = new Number(100)
+// let balance = new Number(100) //declare a nummber with new keyword as an object
 // console.log(balance)
 // console.log( typeof balance)
 
 // ********************Math functions on numbers
 
 // **************toString()
-// let balance = 100  // |^number is declared above with object keyword new 
+// let balance = 100  // |^number is declared above with object keyword "new" 
 // let strBalance=balance.toString()
 // console.log(balance.toString())
 // console.log(strBalance)
@@ -31,7 +31,7 @@ undefined
 // _________________NOTES ON NUMBER AND STRING FUNCTION
 /*  => number declared with new keyword is of object type
     =>when we apply math function like toString() on this object type number , its type is string
-    =>when we apply abobe same operations on number declared normally, its type is number, rest all things are same as with object type number
+    =>when we apply above same operations on number declared normally, its type is number, rest all things are same as with object type number
  */
 
 // ********************toFixed()
@@ -64,11 +64,11 @@ undefined
 79.4
 */
 
-let num4=179.678
-let num5=num4.toPrecision(2)
-let num7=num4.toPrecision(3)
-let num8=num4.toPrecision(4)
-let num9=num4.toPrecision(5)
+// let num4=179.678
+// let num5=num4.toPrecision(2) //gives answer in exponential
+// let num7=num4.toPrecision(3)
+// let num8=num4.toPrecision(4)
+// let num9=num4.toPrecision(5)
 
 // console.log(num5)
 // console.log(num7)
@@ -81,3 +81,44 @@ let num9=num4.toPrecision(5)
 179.7
 179.68*/
 
+// ********************************* toLocaleString()*************
+// let num1=1000000000
+// let num2=num1.toLocaleString();
+// console.log(num2) // giving me value acc to US number system
+
+// let num3=num1.toLocaleString('en-IN') //Indian number system
+// console.log(num3)
+/*__________output
+1,000,000,000
+1,00,00,00,000
+*/
+
+
+// *********************************MATH
+// console.log(Math) //Math is a library in javascript
+// ______output
+// Object [Math] {}
+
+// *************************** abs()
+// let num1=-4
+// console.log(Math.abs(num1)) //  4  gives positive value
+
+// ************round()
+// let num2= 167.8676
+// console.log(Math.round(num2))  // 168  roundoff vaues
+
+// **************ceil() and floor()
+
+// let num=4.1
+// let num2=4.9
+// console.log(Math.ceil(num)) // 5 gives upper values always i.e. 5 (evenif  value is just greater than 4)  )
+// console.log(Math.floor(num2)) // 4 gives lower value i.e. 4 evenif value is too closer to 5
+
+
+// *********************sqrt()
+// let num=4
+// console.log(Math.sqrt(num)) // 2 gives square rrot
+
+
+
+// ***************pow() => gives power
