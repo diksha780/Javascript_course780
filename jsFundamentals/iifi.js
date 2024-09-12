623e9af5706c93b84e369f79b947f2fcfe3b580c
Diksha Nangla <112332164+diksha780@users.noreply.github.com>
2024-09-12 04:20:49 +0000

    sep 12

---
+++ jsFundamentals/iifi.js
@@ -0,0 +1,3 @@
+// *NOTES: => IIFI : Immediately Invoke Function Expressions
+// IIFI is used when we want to immediately invoke the declared function 
+// => it is used because there is problems caused by global pollution, so to remove or prevent that global pollution like globaly declared variable, IIFI is used
